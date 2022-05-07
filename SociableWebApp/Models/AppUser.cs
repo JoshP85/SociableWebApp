c3f59b3c0b2972c3bf788a99e97f4e388a925ccc
JoshP85 <joshua.phillips1985@gmail.com>
2022-05-07 17:26:00 +1000

    Tabs for log in and register, appuser and login models

---
+++ SociableWebApp/Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿namespace SociableWebApp.Models
+{
+    public class AppUser
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Location { get; set; }
+        public DateTime AccCreatedDate { get; set; }
+        public DateTime AccUpdatedDate { get; set; }
+
+    }
+}
